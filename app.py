8bbe8ccc6c4b17da35c20935f1dc6940f9b1a3e1
bsowjanya861-dot <bsowjanya861@gmail.com>
2026-04-03 15:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 from PIL import Image
 from xgboost import XGBClassifier
 def set_bg():
-    url = "https://images.unsplash.com/photo-1526378722484-bd91ca387e72"
+    url = "https://images.unsplash.com/photo-1508780709619-79562169bc64"
     css = f"""
     <style>
     .stApp {{
