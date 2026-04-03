ddcb4a2266ceb8b771bad1c0dbb0af399b67bcf4
bsowjanya861-dot <bsowjanya861@gmail.com>
2026-04-03 14:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 from PIL import Image
 from xgboost import XGBClassifier
 def set_bg():
-    url = "https://attachments/sfQbSyDqs4tnMFPKi68BX.png"  # replace with your image link
+    url = "https://media.istockphoto.com/id/xxxxxxx/vector/brain-stroke-illustration.jpg"  # replace with your image link
 
     css = f"""
     <style>
