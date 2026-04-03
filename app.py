14fc24207401565688169bf0a3c8d7da025e9c9b
bsowjanya861-dot <bsowjanya861@gmail.com>
2026-04-03 14:45:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 from PIL import Image
 from xgboost import XGBClassifier
 def set_bg():
-    url = "https://images.unsplash.com/photo-1581090700227-1e8f0b1f0b52"  # replace with your image link
+    url = "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b"  # replace with your image link
 
     css = f"""
     <style>
